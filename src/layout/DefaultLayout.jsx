c1d38968366fc32e1e36d83a1fb0991043ce0070
Hyun9811 <145583411+Hyun9811@users.noreply.github.com>
2024-11-20 11:58:21 +0900

    fix: 코드 수정

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import AppPages from '../route/AppPages';
 import { BaseLayout, GlobalStyle } from '../global/globalStyle';
-<<<<<<< HEAD
-=======
 import Header from '../components/common/Header/Header';
 import Footer from '../components/common/Footer/Footer';
->>>>>>> dev
+
 
 const DefaultLayout = () => {
   return (
