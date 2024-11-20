89255bc58c8914d941226da070382194313782b8
iam-eungin <1618232@donga.ac.kr>
2024-11-20 15:02:54 +0900

    style: 폰트 수정

--- src/global/globalStyle.js
+++ src/global/globalStyle.js
@@ -25,19 +25,29 @@ export const GlobalStyle = createGlobalStyle`
     font-style: normal;
   }
 
+  @font-face {
+    font-family: 'SUIT-Bold';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_suit@1.0/SUIT-Bold.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+
   h1, h2, h3, h4, h5, h6, p, button {
+    font-family: 'SUIT-Bold', sans-serif;
+  }
+
+  .extra-bold {
     font-family: 'Paperlogy-8ExtraBold', sans-serif; 
+    color: ${colors.white};
   }
 
   .bold {
-    font-family: 'Paperlogy-8ExtraBold', sans-serif; 
-    font-weight: 700;
+    font-family: 'SUIT-Bold', sans-serif; 
     color: ${colors.white};
   }
 
   .regular {
     font-family: 'SUIT-Regular', sans-serif; 
-    font-weight: 400;
     color: ${colors.white};
   }
 `;
@@ -45,13 +55,8 @@ export const GlobalStyle = createGlobalStyle`
 export const BaseLayout = styled.div`
   background-color: ${colors.black};
   width: 100vw;
-  /* height: 100%; */
-  height: 100vh;
+  height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
-`;
-
-export const MainContainer = styled.div`
-  padding: 1.9rem 16.25rem;
 `;
