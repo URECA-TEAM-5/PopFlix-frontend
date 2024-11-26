078554fb570c2bca075fd6ac531401339ec483a5
Hyun9811 <145583411+Hyun9811@users.noreply.github.com>
2024-11-27 00:16:59 +0900

    refactor:더미데이터 data.js로 이동

--- src/components/main/TodayPickMovieList.jsx
+++ src/components/main/TodayPickMovieList.jsx
@@ -1,31 +1,16 @@
 import React,{useState} from 'react';
 import styles from './style/TodayPickMovieList.module.css';
+import {pickContentData} from './data';
 import Slider from "react-slick";
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 const TodayPickMovieList = () => {
-    const pickContentData = [
-        {id:1,movieImage:'https://via.placeholder.com/150'},
-        {id:2,movieImage:'https://via.placeholder.com/150'},
-        {id:3,movieImage:'https://via.placeholder.com/150'},
-        {id:4,movieImage:'https://via.placeholder.com/150'},
-        {id:5,movieImage:'https://via.placeholder.com/150'},
-        {id:6,movieImage:'https://via.placeholder.com/150'},
-        {id:7,movieImage:'https://via.placeholder.com/150'},
-        {id:8,movieImage:'https://via.placeholder.com/150'},
-        {id:9,movieImage:'https://via.placeholder.com/150'},
-        {id:10,movieImage:'https://via.placeholder.com/150'}
-    ]
+    
     const [currentIndex,setCurrentIndex]=useState(0);
     const handleRight = () => {
         if(currentIndex < pickContentData.length -5 ){
             setCurrentIndex(currentIndex + 5);
-        }
-    };
-    const handleLeft = () => {
-        if(currentIndex > 0 ){
-            setCurrentIndex(currentIndex - 5);
         }
     };
     const settings = {
@@ -38,8 +23,8 @@ const TodayPickMovieList = () => {
       };
     return (
         <div className={styles.container}>
-            오늘은 이 콘텐츠 어때요?
-            <Slider {...settings}>
+            <p className={styles.p}>오늘은 이 콘텐츠 어때요?</p>
+            <Slider {...settings} className={styles.slideContainer}>
             {pickContentData.map((item) => (
                 <div key={item.id} className={styles.todayPickMovieCard}>
                     <img src={item.movieImage} className={styles.todayPickMovieCard}/>
