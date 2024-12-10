b9eeaa61575432df6f0ab04a2d960030083f9537
chodonghee-hub <67031025+chodonghee-hub@users.noreply.github.com>
2024-12-10 16:04:36 +0900

    fix: 메인페이지 오류 수정

--- src/components/main/MovieList.jsx
+++ src/components/main/MovieList.jsx
@@ -7,7 +7,7 @@ import heart from '/assets/heart.svg';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import Grid2 from '@mui/material/Grid2';  // Grid2 import
+import Grid2 from '@mui/material/Grid2'; // Grid2 import
 
 const MovieList = () => {
   const [movies, setMovies] = useState([]); // 영화 데이터 상태
@@ -40,20 +40,17 @@ const MovieList = () => {
 
   // 페이지 변경 함수
   const paginate = (pageNumber) => setPage(pageNumber);
-  
+
   const handleMovieDetail = (id) => {
     navigate(`/movieDetail/${id}`);
-  }
+  };
 
   return (
     <div className={styles.container}>
       {/* 장르 버튼 */}
       <div id="genre-buttons-container" className={styles.genreButtonsContainer}>
         <Slider {...settings.current}>
-          <button
-            className={`${styles.genreButton} ${selectedGenre === 'All' ? styles.genreButtonSelected : ''}`}
-            onClick={() => setSelectedGenre('All')}
-          >
+          <button className={`${styles.genreButton} ${selectedGenre === 'All' ? styles.genreButtonSelected : ''}`} onClick={() => setSelectedGenre('All')}>
             전체
           </button>
           {genres.map((genre, index) => (
@@ -73,32 +70,27 @@ const MovieList = () => {
 
       {/* 영화 리스트 */}
       <div className={styles.movieGrid}>
-      <Grid2 container spacing={2} justifyContent="center">
-        {currentMovies.map((movie) => (
-          <Grid2 item xs={12} sm={6} md={4} lg={3} key={movie.id}>
-            <div className={styles.movieCard}>
-              <img 
-              className={styles.movieImage} 
-              src={movie.imageUrl} 
-              alt={movie.title} 
-              onClick={()=> handleMovieDetail(movie.id)}/>
-              <h4 className={styles.movieTitle}>{movie.title}</h4>
-              <div className={styles.movieStats}>
-                <div className={styles.movieRating}>
-                  <img className={styles.movieIcon} src={mainGPA} alt="평점" />
-                  <p>{movie.rating}</p>
-                </div>
-                <div className={styles.movieLikes}>
-                  <img className={styles.movieIcon} src={heart} alt="좋아요" />
-                  <p>{movie.likes}</p>
+        <Grid2 container spacing={2} justifyContent="center">
+          {currentMovies.map((movie) => (
+            <Grid2 xs={12} sm={6} md={4} lg={3} key={movie.id}>
+              <div className={styles.movieCard}>
+                <img className={styles.movieImage} src={movie.imageUrl} alt={movie.title} onClick={() => handleMovieDetail(movie.id)} />
+                <h4 className={styles.movieTitle}>{movie.title}</h4>
+                <div className={styles.movieStats}>
+                  <div className={styles.movieRating}>
+                    <img className={styles.movieIcon} src={mainGPA} alt="평점" />
+                    <p>{movie.rating}</p>
+                  </div>
+                  <div className={styles.movieLikes}>
+                    <img className={styles.movieIcon} src={heart} alt="좋아요" />
+                    <p>{movie.likes}</p>
+                  </div>
                 </div>
               </div>
-            </div>
-          </Grid2>
-        ))}
-      </Grid2>
+            </Grid2>
+          ))}
+        </Grid2>
       </div>
-      
 
       {/* 페이지네이션 */}
       {movies.length > moviesPerPage && (
@@ -110,9 +102,7 @@ const MovieList = () => {
           ))}
         </div>
       )}
-      <button className={styles.viewMoreBtn}>
-        더 보기
-      </button>
+      <button className={styles.viewMoreBtn}>더 보기</button>
 
       {/* 로딩 상태 */}
       {isLoading && <div>Loading...</div>}
