c0b18e206449d2e51e60bd032942c00be162227e
chodonghee-hub <67031025+chodonghee-hub@users.noreply.github.com>
2024-12-14 13:31:59 +0900

    feat: 검색 기능 api 연동

--- src/api/search/search.js
+++ src/api/search/search.js
@@ -2,7 +2,7 @@ import request from '../axios';
 
 const SEARCH_MOVIES_BY_KEYWORD = '/api/movies?page=0&size=10&keyword=';
 
-export const searchMovies = async (keyword) => {
+export const apiGetSearchMovies = async (keyword) => {
   try {
     const response = await request.get(SEARCH_MOVIES_BY_KEYWORD + keyword);
     console.log(response);
