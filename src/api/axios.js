dfb9c2888dca883aa69e8f0be8bca2570297aaae
wijihooooon <dnlwlgns1117@naver.com>
2024-12-15 06:30:34 +0900

    feat:회원가입 연동 및 토큰관리 수정

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const request = axios.create({
   baseURL: 'https://popflix.org',
-  // withCredentials: true,
+  withCredentials: true,
 });
 
 let accessToken = null;
@@ -10,11 +10,15 @@ let accessToken = null;
 // 요청 인터셉터: AccessToken 추가
 request.interceptors.request.use(
   (config) => {
-    if (accessToken) {
-      config.headers.Authorization = `Bearer ${accessToken}`;
-    }
-    return config;
+    return config; // 쿠키에있는 토큰 넘김
   },
+
+  // (config) => {
+  //   if (accessToken) {
+  //     config.headers.Authorization = `Bearer ${accessToken}`;
+  //   }
+  //   return config;
+  // },
   (error) => Promise.reject(error)
 );
 
@@ -24,16 +28,15 @@ request.interceptors.response.use(
   async (error) => {
     if (error.response && error.response.status === 401) {
       try {
-        const refreshResponse = await axios.post('https://popflix.org/auth/refresh', {}, { withCredentials: true });
+        // AccessToken 재발급 요청 (RefreshToken 사용)
+        await axios.post('https://popflix.org/auth/refresh', {}, { withCredentials: true });
 
-        accessToken = refreshResponse.data.accessToken;
-
-        error.config.headers.Authorization = `Bearer ${accessToken}`;
+        // 원래 실패했던 요청 재시도
         return request(error.config);
       } catch (refreshError) {
         console.error('AccessToken 갱신 실패:', refreshError);
 
-        accessToken = null;
+        // 갱신 실패 시 로그아웃 처리
         window.location.href = '/';
         return Promise.reject(refreshError);
       }
@@ -42,17 +45,40 @@ request.interceptors.response.use(
   }
 );
 
+// request.interceptors.response.use(
+//   (response) => response,
+//   async (error) => {
+//     if (error.response && error.response.status === 401) {
+//       try {
+//         const refreshResponse = await axios.post('https://popflix.org/auth/refresh', {}, { withCredentials: true });
+
+//         accessToken = refreshResponse.data.accessToken;
+
+//         error.config.headers.Authorization = `Bearer ${accessToken}`;
+//         return request(error.config);
+//       } catch (refreshError) {
+//         console.error('AccessToken 갱신 실패:', refreshError);
+
+//         accessToken = null;
+//         window.location.href = '/';
+//         return Promise.reject(refreshError);
+//       }
+//     }
+//     return Promise.reject(error);
+//   }
+// );
+
 // AccessToken 초기화 함수 (나중에 최상위 컴포넌트에서 페이지 로드될떄마다 호출하기~)
-const initializeAccessToken = async () => {
-  try {
-    const refreshResponse = await axios.post('https://popflix.org/auth/refresh', {}, { withCredentials: true });
-    accessToken = refreshResponse.data.accessToken;
-    console.log('AccessToken 초기화 완료:', accessToken);
-  } catch (error) {
-    console.error('AccessToken 초기화 실패:', error);
-    accessToken = null;
-    window.location.href = '/';
-  }
-};
+// const initializeAccessToken = async () => {
+//   try {
+//     const refreshResponse = await axios.post('https://popflix.org/auth/refresh', {}, { withCredentials: true });
+//     accessToken = refreshResponse.data.accessToken;
+//     console.log('AccessToken 초기화 완료:', accessToken);
+//   } catch (error) {
+//     console.error('AccessToken 초기화 실패:', error);
+//     accessToken = null;
+//     window.location.href = '/';
+//   }
+// };
 
 export default request;
