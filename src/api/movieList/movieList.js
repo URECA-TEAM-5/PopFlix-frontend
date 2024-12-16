fa8414339e15208b5b1c844e7ff1beaece84b93f
Hyun9811 <145583411+Hyun9811@users.noreply.github.com>
2024-12-16 16:25:21 +0900

    feat: 영화 조회 연동 구현

--- src/api/movieList/movieList.js
+++ src/api/movieList/movieList.js
@@ -1,9 +1,15 @@
 // import request from "../axios";
 import axios from 'axios';
 
-export const getMovieList = async (page,size) => {
-//   const movieData = await axios.get(`https://popflix.org/api/movies?page=${page}&size=${size}`);
-  const movieData = await axios.get('https://popflix.org/api/movies?page=0&size=20');
-  console.log(movieData.data);
+export const getMovieList = async (page,size,genre) => {
+  if(genre == null){
+    genre='';
+  }
+  else if(genre == 'All'){
+    genre='';
+  }
+  // const movieData = await axios.get(`http://localhost:8080/api/movies?page=${page}&size=${size}&genre=${genre}`);
+  const movieData = await axios.get(`https://popflix.org/api/movies?page=${page}&size=${size}&genre=${genre}`);
+//   const movieData = await axios.get('https://popflix.org/api/movies?page=0&size=200');
   return movieData.data;
 };
