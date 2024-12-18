1cd158fee65d9abab426cc2bff2d18fe37218b59
chodonghee-hub <67031025+chodonghee-hub@users.noreply.github.com>
2024-12-18 22:59:41 +0900

    feat: 폰트 사이즈 조절 추가

--- src/components/common/emptyResult/EmptyResult.jsx
+++ src/components/common/emptyResult/EmptyResult.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { EmptyResultImage, EmptyResultWrapper } from './style/EmptyResultStyle';
 
-const EmptyResult = ({ img, message, description, size, p }) => {
+const EmptyResult = ({ img, message, description, size, p, fontSize }) => {
   return (
-    <EmptyResultWrapper $padding={p}>
+    <EmptyResultWrapper $padding={p} $fontSize={fontSize}>
       <EmptyResultImage className="img__empty" $size={size} src={img ? img : '/assets/review_null.svg'} alt="empty images" />
       <p className="text__message">{message ? message : 'message'}</p>
       {description && <p className="text__description">{description}</p>}
