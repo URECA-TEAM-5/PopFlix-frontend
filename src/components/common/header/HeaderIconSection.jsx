f4be279bbd9796a3ea8a58d612671b91c1aafe84
chodonghee-hub <67031025+chodonghee-hub@users.noreply.github.com>
2024-12-18 16:11:10 +0900

    fix: 헤더 리렌더링 문제 및 회원 정보 조회 수정

--- src/components/common/header/HeaderIconSection.jsx
+++ src/components/common/header/HeaderIconSection.jsx
@@ -1,6 +1,6 @@
 import { faBell, faMoon, faUser } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { IconSection } from './style/HeaderStyle';
 import { Link } from 'react-router-dom';
 import LoginModal from '../../Login/LoginModal';
@@ -10,12 +10,20 @@ import { chkUserInfo } from '../../userInfo/modal/chkUserInfo';
 const HeaderIconSection = () => {
   const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
   const [isMyInfoModalOpen, setIsMyInfoModalOpen] = useState(false);
+  const isLoaded = useRef(false);
+  const chkLogin = useRef('');
+
+  if (!isLoaded.current) {
+    isLoaded.current = true;
+    chkLogin.current = chkUserInfo();
+  }
+
   return (
     <>
       <IconSection>
         <div className="inner__section">
           <FontAwesomeIcon className="icon" icon={faMoon} size="xl" />
-          {chkUserInfo() && (
+          {chkLogin.current && (
             <Link to="/alarm">
               <FontAwesomeIcon className="icon" icon={faBell} size="xl" />
             </Link>
@@ -23,7 +31,7 @@ const HeaderIconSection = () => {
           {sessionStorage.getItem('accessToken') ? (
             <>
               {(() => {
-                const user = JSON.parse(sessionStorage.getItem('user')); // User 정보 가져오기
+                const user = chkUserInfo(); // User 정보 가져오기
                 const profileImage = user?.profileImage || user?.data?.defaultProfileImage; // profileImage 우선 사용
 
                 return <img className="profile" src={profileImage} alt="profile" onClick={() => setIsMyInfoModalOpen(true)} style={{ cursor: 'pointer' }} />;
