d0203e7995e01a063b9631e1f575715cc1da4271
iam-eungin <1618232@donga.ac.kr>
2024-12-19 18:08:32 +0900

    fix: 다른 보관함 목록 캐러셀 변경

--- src/components/watchlistDetail/style/WatchListDetailOtherStorage.js
+++ src/components/watchlistDetail/style/WatchListDetailOtherStorage.js
@@ -2,61 +2,55 @@ import { styled } from 'styled-components';
 
 export const OtherList = styled.div`
     width: 100%;
-    height: 18rem;
     h4 {
         padding: 1rem 0;
     }
-    .imageContainer {
-        display: flex;
-        gap: 1rem;
+    .carousel {
+        margin-top: 1rem;
+        margin: 0 auto;
+        width: 95%;
     }
     .imageWrapper {
-        width: 7.5rem;
-        height: 7.5rem;
-        position: relative;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        text-align: center;
+        padding-left: 1rem;
     }
     .image {
-        width: 100%;
-        height: 100%;
+        width: 7.5rem;
+        height: 7.5rem;
         object-fit: cover;
         border-radius: 0.313rem;
     }
     .imagePlaceholder {
-        width: 100%;
-        height: 100%;
+        width: 7.5rem;
+        height: 7.5rem;
         background-color: #f0f0f0;
         display: flex;
         justify-content: center;
         align-items: center;
         border-radius: 0.313rem;
     }
-    .textContainer {
-        width: 100%;
-        display: flex;
-        gap: 0.5rem;
-        div {
-            width: 7.5rem;
-        }
+    .textWrapper {
+        width: 7.5rem;
+        margin-top: 0.5rem;
     }
     .title {
-        height: 3rem;
         font-size: 0.875rem;
-        text-align: left;
+        height: 2.5rem;
     }
     .count {
-        width: 100%;
         display: flex;
         justify-content: space-between;
         align-items: center;
         font-size: 0.875rem;
     }
-
     .likeCount {
         display: flex;
         justify-content: flex-end;
         align-items: center;
     }
-
     .heartlike {
         color: red;
     }
